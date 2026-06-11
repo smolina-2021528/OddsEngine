c6a239189bc68f8b18e833bb673517a405014af8
smolina-2021528 <smolina-2021528@kinal.edu.gt>
2026-06-10 21:11:49 -0600

    Create collector package structure

---
+++ app/collector/__init__.py
@@ -0,0 +1,6 @@
+"""Data collection package.
+
+This package contains the football data collection layer used to retrieve,
+parse, and prepare raw sports data before it is normalized into the canonical
+database models.
+"""
---
+++ app/collector/scrapers/__init__.py
@@ -0,0 +1,5 @@
+"""Scrapers package.
+
+Scrapers are responsible for retrieving and parsing raw data from public sports
+data sources.
+"""
